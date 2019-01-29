00f5011800c4ba1c1f85c716bd03cce212b239f3
Acorp <39213080+acorpeugen@users.noreply.github.com>
2019-01-30 00:28:30 +0200

    Update App.js
    Add new components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ExpressionInput from './components/expression-input/ExpressionInput';
+import ClearButton from './components/operations-buttons/ClearButton';
+import PercentButton from './components/operations-buttons/PercentButton';
+import PlusMinusButton from './components/operations-buttons/PlusMinusButton';
+import DigitButtons from './components/digit-buttons/DigitButtons';
+import Divide from './components/operations-buttons/Divide';
+import Multiply from './components/operations-buttons/Multiply';
+import Minus from './components/operations-buttons/Minus';
+import Plus from './components/operations-buttons/Plus';
+import Equally from './components/operations-buttons/Equally';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="iphone-x">
+          {/* Iphone-x elements for visualisation controls */}
+          <div className="visor">
+            <span className="infrared-camera"/>
+            <span className="proximity-sensor"/>
+            <span className="speaker"/>
+            <span className="camera"/>
+            <span className="dot-projector"/>
+          </div>
+          <div className="volume-button"/>
+          <div className="power-button"/>
+
+          <div className="calculator">
+            <ExpressionInput/>
+            <ClearButton/>
+            <PercentButton/>
+            <PlusMinusButton/>
+            <DigitButtons/>
+            <Divide/>
+            <Multiply/>
+            <Minus/>
+            <Plus/>
+            <Equally/>
+          </div>
+        </div>
     );
   }
 }
