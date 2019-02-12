55f31e3f425a9c4947ea4233086bd0f697a0f27b
Acorp <39213080+acorpeugen@users.noreply.github.com>
2019-02-12 23:55:33 +0200

    Create new component ExpressionInput
    Add new styles for this component

---
+++ src/Calculator/components/ExpressionInput/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import './index.scss';
+
+export default class ExpressionInput extends Component {
+  render() {
+    return <div className="expression-input">0</div>
+  }
+}
