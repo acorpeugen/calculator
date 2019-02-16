70dbd6223e1c62a8a58be03723f967ca0e31a586
Acorp <39213080+acorpeugen@users.noreply.github.com>
2019-02-16 03:54:59 -0800

    Move calculator add this

--- src/App.js
+++ src/App.js
@@ -5,101 +5,101 @@ import './App.scss';
 
 
 export default class App extends Component {
+  
+  calculator = [
+    {
+      id: 1,
+      functionData: [
+        {
+          id: 1,
+          value: 'AC'
+        },
+        {
+          id: 2,
+          value: '%'
+        },
+        {
+          id: 3,
+          value: '+/-'
+        }
+      ],
+      operationData: [
+        {
+          id: 1,
+          value: '/'
+        },
+        {
+          id: 2,
+          value: '*'
+        },
+        {
+          id: 3,
+          value: '-'
+        },
+        {
+          id: 4,
+          value: '+'
+        },
+        {
+          id: 5,
+          value: '='
+        }
+      ],
+      digitData: [
+        {
+          id: 1,
+          value: 7
+        },
+        {
+          id: 2,
+          value: 8
+        },
+        {
+          id: 3,
+          value: 9
+        },
+        {
+          id: 4,
+          value: 4
+        },
+        {
+          id: 5,
+          value: 5
+        },
+        {
+          id: 6,
+          value: 6
+        },
+        {
+          id: 7,
+          value: 1
+        },
+        {
+          id: 8,
+          value: 2
+        },
+        {
+          id: 9,
+          value: 3
+        },
+        {
+          id: 10,
+          value: 0
+        },
+        {
+          id: 11,
+          value: '.'
+        },
+      ]
+    }
+  ];
 
   render() {
-
-    const calculator = [
-      {
-        id: 1,
-        functionData: [
-          {
-            id: 1,
-            value: 'AC'
-          },
-          {
-            id: 2,
-            value: '%'
-          },
-          {
-            id: 3,
-            value: '+/-'
-          }
-        ],
-        operationData: [
-          {
-            id: 1,
-            value: '/'
-          },
-          {
-            id: 2,
-            value: '*'
-          },
-          {
-            id: 3,
-            value: '-'
-          },
-          {
-            id: 4,
-            value: '+'
-          },
-          {
-            id: 5,
-            value: '='
-          }
-        ],
-        digitData: [
-          {
-            id: 1,
-            value: 7
-          },
-          {
-            id: 2,
-            value: 8
-          },
-          {
-            id: 3,
-            value: 9
-          },
-          {
-            id: 4,
-            value: 4
-          },
-          {
-            id: 5,
-            value: 5
-          },
-          {
-            id: 6,
-            value: 6
-          },
-          {
-            id: 7,
-            value: 1
-          },
-          {
-            id: 8,
-            value: 2
-          },
-          {
-            id: 9,
-            value: 3
-          },
-          {
-            id: 10,
-            value: 0
-          },
-          {
-            id: 11,
-            value: '.'
-          },
-        ]
-      }
-    ];
 
     return (
       <div className="phone">
         <Controls/>
-        {calculator.map(calc => <Calculator calc={calc} key={calc.id} />)}
+        {this.calculator.map(calc => <Calculator calc={calc} key={calc.id} />)}
       </div>
     );
   }
