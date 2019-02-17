e2f8a8275c7375a81b85db084324fe5b709e5ba2
Acorp <39213080+acorpeugen@users.noreply.github.com>
2019-02-18 01:28:20 +0200

    Add state for input and function onChange

--- src/Calculator/components/ExpressionInput/index.js
+++ src/Calculator/components/ExpressionInput/index.js
@@ -2,7 +2,17 @@ import React, {Component} from 'react';
 import './index.scss';
 
 export default class ExpressionInput extends Component {
+  state = {
+    inputValue: 0,
+  };
+
+  onInputChange = () => {
+    console.log(`Change input value`);
+  };
+
   render() {
-    return <div className="expression-input">0</div>
+    const {inputValue} = this.state;
+
+    return <div className="expression-input" onChange={this.onInputChange}>{inputValue}</div>
   }
 }
