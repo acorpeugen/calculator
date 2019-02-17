df44e33b1287f561328633875696ac55608620d8
Acorp <39213080+acorpeugen@users.noreply.github.com>
2019-02-18 01:26:43 +0200

    Add operationData map

--- src/Calculator/components/OperationButtons/index.js
+++ src/Calculator/components/OperationButtons/index.js
@@ -5,21 +5,12 @@ export default class OperationButtons extends Component {
   render() {
 
     const {
-      value,
-      onDivide,
-      onMultiply,
-      onMinus,
-      onPlus,
-      onEqually
+      operationData
     } = this.props;
 
     return (
       <div className="operation-buttons">
-        <button type="button" value="/" className="btn btn-orange" onClick={onDivide}>{}</button>
-        <button type="button" value="*" className="btn btn-orange" onClick={onMultiply}>{}</button>
-        <button type="button" value="-" className="btn btn-orange" onClick={onMinus}>{}</button>
-        <button type="button" value="+" className="btn btn-orange" onClick={onPlus}>{}</button>
-        <button type="button" value="=" className="btn btn-orange" onClick={onEqually}>{}</button>
+        {operationData.map(operation => <button key={operation.id} className="btn btn-orange">{operation.value}</button>)}
       </div>
     );
   }
