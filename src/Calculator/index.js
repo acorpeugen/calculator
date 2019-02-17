d917a35d0f379a27130fc579cab3e21be1cfe62c
Acorp <39213080+acorpeugen@users.noreply.github.com>
2019-02-18 01:26:19 +0200

    Update index.js
    Remove functions not needed

--- src/Calculator/index.js
+++ src/Calculator/index.js
@@ -12,67 +12,14 @@ export default class Calculator extends Component {
     const {
       calc
     } = this.props;
-  
-    console.log(OperationButtons);
 
     return (
       <div className="calculator" key={calc.id}>
         <ExpressionInput/>
-        <FunctionButtons
-          onClear={this.Clear}
-          onPercent={this.Percent}
-          onPlusMinus={this.PlusMinus}
-        />
+        <FunctionButtons functionData={calc.functionData}/>
         <DigitButtons digitData={calc.digitData} />
-        <OperationButtons
-          value={this.value}
-          onDivide={this.Divide}
-          onMultiply={this.Multiply}
-          onMinus={this.Minus}
-          onPlus={this.Plus}
-          onEqually={this.Equally}
-        />
+        <OperationButtons operationData={calc.operationData}/>
       </div>
     );
-  };
-  
-
-  /* Function Buttons Verification Handel Click */ 
-  Clear = () => {
-    console.log(`Clear: ${this.props.calc.id}`);
-  };
-
-  Percent = () => {
-    console.log(`Percent: ${this.props.calc.id}`);
-  };
-
-  PlusMinus = () => {
-    console.log(`Plus Minus: ${this.props.calc.id}`);
-  };
-
-  /* Digit Buttons Verification Handel Click */
-  Digit = () => {
-    console.log(`Digit: ${this.props.calc.id}`);
-  };
-
-  /* Operation Buttons Verification Handel Click */
-  Divide = () => {
-    console.log(`Divide: ${this.props.calc.id}`);
-  };
-
-  Multiply = () => {
-    console.log(`Multiply: ${this.props.calc.id}`);
-  };
-
-  Minus = () => {
-    console.log(`Minus: ${this.props.calc.id}`);
-  };
-
-  Plus = () => {
-    console.log(`Plus: ${this.props.calc.id}`);
-  };
-
-  Equally = () => {
-    console.log(`Equally: ${this.props.calc.id}`);
   };
 }
